2d1b86bd8eabd5a72affd2c3cb096a40710f101f
Mahmoud Omar <Mahmoud.Omar@itworx.com>
2019-06-19 12:46:25 +0200

    QnA-Bot

---
+++ myqnamakerrr-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=myqnamakerrr-bot-Web-Deploy.pubxml -p:Password=ESRzcBA63AgLHp5MqYmc9A0ulFCo6RdwsCfkfwPl44Ssa5ScmkhCh5knxQSe
+
